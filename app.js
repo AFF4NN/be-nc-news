bf4e11956564836fcc30f35b92cdebcb56760d0d
AFF4NN <161034165+AFF4NN@users.noreply.github.com>
2024-05-31 14:39:38 +0100

    built article comments endpoint from most recent comments and tested for errors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const { getAllTopics } = require("./controllers/topics.controllers");
 const { getApi } = require("./controllers/api.controllers");
 const { getArticleById, getAllArticles  } = require("./controllers/articles.controllers");
-
+const {getArticleComments} = require("./controllers/comments.controllers")
 
 app.get("/api/topics", getAllTopics);
 
@@ -12,6 +12,8 @@ app.get("/api", getApi);
 app.get("/api/articles/:article_id", getArticleById);
 
 app.get("/api/articles", getAllArticles);
+
+app.get("/api/articles/:article_id/comments", getArticleComments)
 
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
